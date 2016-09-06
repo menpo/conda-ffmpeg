67146835f1977bf27726268ebdc31630959518e5
Patrick Snape <patricksnape@gmail.com>
2016-09-07 00:19:00 +0100

    Update Windows script - don't try creating SCRIPTS

--- conda/bld.bat
+++ conda/bld.bat
@@ -1,9 +1,5 @@
 7z x *.7z
 
-rem Not sure why we have to do this?
-rm %SCRIPTS%
-mkdir %SCRIPTS%
-
 copy "ffmpeg-2.7-win%ARCH%-static\bin\ffmpeg.exe" "%SCRIPTS%\ffmpeg.exe"
 copy "ffmpeg-2.7-win%ARCH%-static\bin\ffplay.exe" "%SCRIPTS%\ffplay.exe"
 copy "ffmpeg-2.7-win%ARCH%-static\bin\ffprobe.exe" "%SCRIPTS%\ffprobe.exe"
