c417b7e024c3310c59cf0ce4c05ca39141f66c6a
Patrick Snape <patricksnape@gmail.com>
2016-09-07 00:23:20 +0100

    Add PKG_VERSION to windows builds

--- conda/bld.bat
+++ conda/bld.bat
@@ -1,7 +1,7 @@
 7z x *.7z
 
-copy "ffmpeg-2.7-win%ARCH%-static\bin\ffmpeg.exe" "%SCRIPTS%\ffmpeg.exe"
-copy "ffmpeg-2.7-win%ARCH%-static\bin\ffplay.exe" "%SCRIPTS%\ffplay.exe"
-copy "ffmpeg-2.7-win%ARCH%-static\bin\ffprobe.exe" "%SCRIPTS%\ffprobe.exe"
+copy "ffmpeg-%PKG_VERSION%-win%ARCH%-static\bin\ffmpeg.exe" "%SCRIPTS%\ffmpeg.exe"
+copy "ffmpeg-%PKG_VERSION%-win%ARCH%-static\bin\ffplay.exe" "%SCRIPTS%\ffplay.exe"
+copy "ffmpeg-%PKG_VERSION%-win%ARCH%-static\bin\ffprobe.exe" "%SCRIPTS%\ffprobe.exe"
 
 if errorlevel 1 exit 1
